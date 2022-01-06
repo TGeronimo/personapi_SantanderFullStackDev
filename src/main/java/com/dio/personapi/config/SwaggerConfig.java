7f7af44301f37732bbc0e8a3c7a10a528d230359
tgeronimo <thi.matheus@gmail.com>
2022-01-06 18:59:47 -0300

    added project documentation using Swagger

---
+++ src/main/java/com/dio/personapi/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.dio.personapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.dio.personapi.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(buildApiInfo());
+    }
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Person API")
+                .description("API REST para gerenciamento de pessoas")
+                .version("1.0.0")
+                .contact(new Contact(
+                        "Thiago Geronimo",
+                        "github/tgeronimo",
+                        null))
+                .build();
+    }
+
+}
